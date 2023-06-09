105554b5c02a84540d4767fe4910dbb2e4cfe5bf
flyinghorse0510 <mhy@flyinghorse.top>
2023-06-09 19:32:07 +0800

    add author info && remove useless file

--- scripts/cloudlab/start_onenode_vhive_cluster.go
+++ scripts/cloudlab/start_onenode_vhive_cluster.go
@@ -1,9 +1,32 @@
+// MIT License
+//
+// Copyright (c) 2023 Haoyuan Ma and vHive team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package cloudlab
 
 import (
 	"time"
 
 	cluster "github.com/vhive-serverless/vHive/scripts/cluster"
+	"github.com/vhive-serverless/vHive/scripts/configs"
 	utils "github.com/vhive-serverless/vHive/scripts/utils"
 )
 
@@ -70,7 +93,7 @@ func StartOnenodeVhiveCluster(sandBox string) error {
 
 	// Build vHive
 	utils.WaitPrintf("Building vHive")
-	_, err = utils.ExecShellCmd("cd %s && source /etc/profile && go build")
+	_, err = utils.ExecShellCmd("cd %s && source /etc/profile && go build", configs.VHive.VHiveRepoPath)
 	if !utils.CheckErrorWithTagAndMsg(err, "Failed to build vHive!\n") {
 		return err
 	}
