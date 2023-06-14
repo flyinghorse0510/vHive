acf5afdcb811a84975b5a184e95bb8fa06e08017
flyinghorse0510 <mhy@flyinghorse.top>
2023-06-15 07:24:06 +0800

    add unix prefix for criSocket

--- scripts/cluster/create_one_node_cluster.go
+++ scripts/cluster/create_one_node_cluster.go
@@ -97,7 +97,7 @@ func CreateOneNodeKubernetes(criSock string) error {
 	} else {
 		// On a non container environment
 		utils.WaitPrintf("Creating cluster")
-		_, err = utils.ExecShellCmd(`sudo kubeadm init --ignore-preflight-errors=all --cri-socket %s --pod-network-cidr=unix://'%s'`, criSock, configs.Kube.PodNetworkCidr)
+		_, err = utils.ExecShellCmd(`sudo kubeadm init --ignore-preflight-errors=all --cri-socket unix://'%s' --pod-network-cidr=%s`, criSock, configs.Kube.PodNetworkCidr)
 		if !utils.CheckErrorWithTagAndMsg(err, "Failed to create cluster!\n") {
 			return err
 		}
